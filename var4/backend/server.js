0bf0fe4540750f7375fc47c04e48d26fba564fe6
Preston Gilmore <prestongilmore@Prestons-MacBook-Pro.local>
2025-02-03 09:20:48 -0700

    sending latest updates

--- var4/backend/server.js
+++ var4/backend/server.js
@@ -8,27 +8,32 @@ const users = require('./data/users');
 // Express app
 const app = express();
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+// Set the directory for views
+app.set('views', __dirname + '/views'); 
+
 // Custom Template Engine
-app.engine('custom', (filePath, options, callback) => {
-    fs.readFile(filePath, 'utf-8', (err, content) => {
-        if (err) return callback(err);
+// app.engine('custom', (filePath, options, callback) => {
+//     fs.readFile(filePath, 'utf-8', (err, content) => {
+//         if (err) return callback(err);
 
-        let rendered = content;
+//         let rendered = content;
 
-        // Update this regex to handle spaces inside {{ }}
-        Object.keys(options).forEach((key) => {
-            const regex = new RegExp(`{{\\s*${key}\\s*}}`, 'g'); 
-            rendered = rendered.replace(regex, options[key]);
-        });
+//         // Update this regex to handle spaces inside {{ }}
+//         Object.keys(options).forEach((key) => {
+//             const regex = new RegExp(`{{\\s*${key}\\s*}}`, 'g'); 
+//             rendered = rendered.replace(regex, options[key]);
+//         });
 
-        return callback(null, rendered);
-    });
-});
+//         return callback(null, rendered);
+//     });
+// });
 
 // Set views directory and use the custom template engine
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 
-app.set('view engine', 'custom');
+// app.set('view engine', 'custom');
 
 // Serve static files (Make sure styles.css is inside /styles)
 app.use(express.static('styles'));
@@ -56,8 +61,24 @@ app.get('/users', (req, res) => {
     });
 });
 
+
+app.get('/', (req, res) => {
+    console.log('hello im talking to you')
+
+    let userList = users
+    .map(user => `<li>${user.name} - ${user.email} (${user.age} years old)</li>`)
+    .join('');
+
+    res.render('index', { 
+        title: "Hello World!",
+        description: "A dynamically generated list of users.",
+        userList: userList,
+    });
+});
+
 // Start Server
 const PORT = process.env.PORT || 4040;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
